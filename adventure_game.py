62b79ddc9126fdf9b795d3eeac9b270d2f2a5235
Loren Waddle <lorenwaddle@Lorens-MacBook-Pro.local>
2020-09-30 22:06:18 -0700

    added display_scene() about to fix print_slowly()

--- adventure_game.py
+++ adventure_game.py
@@ -1,5 +1,6 @@
 import dice
 import sys
+import csv
 from time import sleep
 
 MESSAGE_PAUSE_LENGTH = 1
@@ -8,7 +9,15 @@ SLOW_TEXT_SPEED = 0.01
 def message_pause():
     sleep(MESSAGE_PAUSE_LENGTH)
 
-def display_scenario(scenario):
+##################################################################
+# This idea will probably work but now I need to change          #
+# the print_slowly function because the txt files will have      #
+# there own newlines. This simplifies the print_slowly function  # 
+##################################################################
+def display_scene(scene: str):
+    with open("./scenes/0.txt", "r") as current_scene:
+        for line in current_scene:
+            print_slowly(line)
     # TODO
     return
 
@@ -47,11 +56,9 @@ def print_slowly(text, pause_sentences = True, line_spacing = 1):
         i += 1
 
 
-test = """You discover a responsive website that looks beautiful on both mobile and desktop devices.\
-You spend some time clicking around and come across the Udacity Program Catalog.\
-There are so many programs! You have an interest in two of the programs - \
-Full Stack Developer and iOS Developer. To help you decide which route to take, you roll a pair of dice.\
-If the sum of the to dice is even, become a Full Stack Developer. If the sum of the dice is odd, \
-become an iOS Developer."""
-
-print_slowly(test)
+# test = """You discover a responsive website that looks beautiful on both mobile and desktop devices.\
+# You spend some time clicking around and come across the Udacity Program Catalog.\
+# There are so many programs! You have an interest in two of the programs - \
+# Full Stack Developer and iOS Developer. To help you decide which route to take, you roll a pair of dice.\
+# If the sum of the to dice is even, become a Full Stack Developer. If the sum of the dice is odd, \
+# become an iOS Developer."""
