bb442bd7638b0ff2c14003f28ccd16a0b7401e89
Loren Waddle <lorenwaddle@Lorens-MacBook-Pro.local>
2020-10-01 11:08:27 -0700

    fixed get_decision()

--- adventure_game.py
+++ adventure_game.py
@@ -5,6 +5,17 @@ from time import sleep
 MESSAGE_PAUSE_LENGTH = 1
 SLOW_TEXT_SPEED = 0.01
 
+def display_intro():
+    print("\n" * 500)
+    print("""
+  __  __   __         _ __         ___     __              __              
+ / / / /__/ /__ _____(_) /___ __  / _ |___/ /  _____ ___  / /___ _________ 
+/ /_/ / _  / _ `/ __/ / __/ // / / __ / _  / |/ / -_) _ \/ __/ // / __/ -_)
+\____/\_,_/\_,_/\__/_/\__/\_, / /_/ |_\_,_/|___/\__/_//_/\__/\_,_/_/  \__/ 
+                         /___/                                             
+    """)
+    print("\n" * 3)
+
 def message_pause():
     sleep(MESSAGE_PAUSE_LENGTH)
 
@@ -13,32 +24,31 @@ def message_pause():
 # the print_slowly function because the txt files will have      #
 # there own newlines. This simplifies the print_slowly function  # 
 ##################################################################
-def display_scene(txt_file):
-    with open(txt_file, "r") as current_scene:
+def display_scene(scene_txt_file):
+    with open(scene_txt_file, "r") as current_scene:
         for line in current_scene:
             print_slowly(line)
     # TODO
     return
 
-def get_decision(option_1, option_2 = "", option_3 = ""):
-    options = 1 # increments if options 2 and 3 are used
-    print(f"Press 1: {option_1}")
-    if option_2:
-        print(f"Press 2: {option_2}")
-        options += 1
-    if option_3:
-        print(f"Press 3: {option_3}")
-        options += 1
-    
-    decision = input(": ")
-    if decision.isdigit() == True:
-        decision = int(decision)
-    while decision not in range(1, options + 1):
-        decision = input("Invalid... Try again: ")
-        if decision.isdigit() == True:
-            decision = int(decision)
+def get_decision(options_txt_file):
+    with open(options_txt_file, "r") as options:
+        print("\n") # Give a little space between scence and options
+        lines = options.readlines()
+        for line in lines:
+            print(line.strip())
+        option_count = len(lines)
+        message = ": "
+        while True:
+            decision = input(message)
+            if decision.isdigit() == True:
+                decision = int(decision)
+                if decision in range(1, option_count + 1):
+                    break
+            else:
+                message = "Invalid. Try again: "
 
-    return decision
+        return decision
 
 
 def print_slowly(text, pause_sentences = True):
@@ -50,4 +60,6 @@ def print_slowly(text, pause_sentences = True):
         print(c, sep='', end='', flush=True); sleep(SLOW_TEXT_SPEED)
 
 
+display_intro()
 display_scene("./scenes/0_scene.txt")
+print(get_decision("./scenes/0_options.txt"))
