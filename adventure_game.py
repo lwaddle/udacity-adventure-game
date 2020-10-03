6d09a415c5176bab78a9cbf9b694cb95e3d02791
Loren Waddle <lorenwaddle@Lorens-MacBook-Pro.local>
2020-10-03 13:03:18 -0700

    new model is working better.

--- adventure_game.py
+++ adventure_game.py
@@ -1,100 +1,35 @@
-import dice
-import sys
-import csv
-from time import sleep
+import os
 
-MESSAGE_PAUSE_LENGTH = 1
-SLOW_TEXT_SPEED = 0.01
+def main():
+    game_file_path = "./game_files/"
 
-current_scene = 0   # Used as reference for scene text files
+    game_model = GameModel(game_file_path)
+    print(game_model._game_data)
 
-def control_scene_flow(current_scene: int):
-    scene_file_path = "./scenes/" + str(current_scene) + "_scene.txt"
-    options_file_path = "./scenes/" + str(current_scene) + "_options.txt"
-    options_map_path = "./scenes/" + str(current_scene) + "_options_map.csv"
-    
-    display_scene(scene_file_path)
-    decision = get_decision(options_file_path)
+class GameModel:
+    def __init__(self, game_file_path):
+        self._game_file_path = game_file_path
+        self._game_data = self._load_game_data()
 
-    map_dict = {}
-    with open(options_map_path) as f:
-        map = csv.DictReader(f)
-        map_dict = next(map)
-    
-    print(map_dict)
+    def _load_game_data(self):
+        """Loads the game files into a dict that uses the filename as a key
+        and the actual file as a value"""
+        try:
+            scene_files = os.listdir(self._game_file_path)
+            game_data = {}
+            for file in scene_files:
+                # Populate game_data dict with the filename as key
+                # and the file as value.
+                key = str(file).rsplit(".")[0]
+                game_data[key] = file
 
-    # TODO
-    # if current_scene == 99: # Recursive base case
-    #     print("fjas;lfjas;lfjasdf;lkj") # Exit the function and print some sort of exit message
-    # else:
-    #     clear_screen()
-    #     control_scene_flow(current_scene)   # Use recursion to stack the function
-
-
-def clear_screen():
-    print("\n" * 500)
-
-def display_intro():
-    clear_screen()
-    print("""
-  __  __   __         _ __         ___     __              __              
- / / / /__/ /__ _____(_) /___ __  / _ |___/ /  _____ ___  / /___ _________ 
-/ /_/ / _  / _ `/ __/ / __/ // / / __ / _  / |/ / -_) _ \/ __/ // / __/ -_)
-\____/\_,_/\_,_/\__/_/\__/\_, / /_/ |_\_,_/|___/\__/_//_/\__/\_,_/_/  \__/ 
-                         /___/                                             
-    """)
-    print("\n" * 3)
-
-def message_pause():
-    sleep(MESSAGE_PAUSE_LENGTH)
-
-##################################################################
-# This idea will probably work but now I need to change          #
-# the print_slowly function because the txt files will have      #
-# there own newlines. This simplifies the print_slowly function  # 
-##################################################################
-def display_scene(scene_txt_file):
-    with open(scene_txt_file, "r") as current_scene:
-        for line in current_scene:
-            print_slowly(line)
-    # TODO
-    return
-
-def get_decision(options_txt_file):
-    with open(options_txt_file, "r") as options:
-        print("\n") # Give a little space between scence and options
-        lines = options.readlines()
-        for line in lines:
-            print(line.strip())
-        option_count = len(lines)
-        message = ": "
-        invalid_message = "Invalid. Try again: "
-        while True:
-            decision = input(message)
-            if decision.isdigit() == True:
-                decision = int(decision)
-                if decision in range(1, option_count + 1):
-                    break
-                else:
-                    message = invalid_message
+        except:
+            if os.path.isdir(self._game_file_path) == False:
+                print("Error: game_file_path is invalid")
             else:
-                message = invalid_message
+                print("Error")
+
+        return game_data
 
 
-        return decision
-
-
-def print_slowly(text, pause_sentences = True):
-    """Creates a classic scroll effect on the text as it prints to the screen"""
-    for c in text:
-        if c == "\n":
-            if pause_sentences == True:
-                message_pause()
-        print(c, sep='', end='', flush=True); sleep(SLOW_TEXT_SPEED)
-
-
-
-
-
-display_intro()
-control_scene_flow(0)
+main()
